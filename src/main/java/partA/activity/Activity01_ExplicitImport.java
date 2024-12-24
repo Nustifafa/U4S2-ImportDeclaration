b99ccf717fbc56776ba39f9555cd43155f939670
Nustifafa <Ngyane@icloud.com>
2024-12-24 13:57:49 -0500

    edits

--- src/main/java/partA/activity/Activity01_ExplicitImport.java
+++ src/main/java/partA/activity/Activity01_ExplicitImport.java
@@ -1,4 +1,6 @@
 package partA.activity;
 
+import java.util.Scanner;
+
 public class Activity01_ExplicitImport {
 }
--- src/main/java/partA/example/Example01_ExplicitImport.java
+++ src/main/java/partA/example/Example01_ExplicitImport.java
@@ -1,12 +1,13 @@
-package partA.example;
+package partA.example; //package with classes for basic GUI elements and graphics
 
-import java.util.Scanner;
-
-public class Example01_ExplicitImport {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Hello user, please enter a number.");
-        int numEntered = sc.nextInt();
-        System.out.println("You entered " + numEntered);
+import java.util.Scanner; //to use the Scanner class to read a keyboard input, the qualified name for the scanner class is java.util.Scanner, defined in the java.util package
+//you can avoid the fully qualified class name by using the import statement
+//package is java.util, class is Scanner, this is importing a single class
+public class Example01_ExplicitImport { //beginning of class definition
+    public static void main(String[] args) { //domain
+        Scanner sc = new Scanner(System.in); //using an input of something
+        System.out.println("Hello user, please enter a number."); //output of something
+        int numEntered = sc.nextInt(); //user must input a number
+        System.out.println("You entered " + numEntered); //outputs what the user inputted and a string of text
     }
 }
